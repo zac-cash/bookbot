a688c4e7696553cb0ce76aa94791d55ea1211ddc
zac-cash <Personal@zac.cash>
2024-09-02 15:11:52 -0500

    further refactoring before lesson 10. removed varable char_dict as the script was reusing the variable in two places.

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ path_to_file = './books/frankenstein.txt'
 
 def main():
     bookcontents = Get_Text(path_to_file)
-    w_count = Word_Count(bookcontents)
-    char_count = Char_Count(bookcontents)
+    char_list = Create_Char_List(Char_Count(bookcontents))
+    char_list.sort(reverse=True,key=sort_on)
 
-    print(f"Sup. Welcome to the book report. We are working on the file: {path_to_file}\n")
-    print(f"There are {w_count} words in the book.")
+    print(f"Sup. Welcome to the book report. \nWe are working on the file: {path_to_file.split('/')[-1]}\n")
+    print(f"There are {Word_Count(Get_Text(path_to_file))} words in the book.")
     print("The following is a count of each letter in the book:")
-    print(char_count)
+    for char_dict in char_list:
+        if char_dict['char'].isalpha():
+            print(f"The '{char_dict['char']}' was found {char_dict['num']} times.")
+    print("--- End Book report ---")
+
+
+def Get_Text (filepath):
+        with open(path_to_file) as f:
+            filecontents = f.read()
+        return filecontents
 
 def Word_Count (string):
     return len(string.split())
@@ -22,8 +31,17 @@ def Char_Count (string):
             returning_dict[char] += 1
     return returning_dict
 
-def Get_Text (filepath):
-        with open(path_to_file) as f:
-            filecontents = f.read()
-        return filecontents
+def Create_Char_List (dict):
+    list_of_char = []
+    for key in dict:
+        list_of_char.append(
+            {
+                "char": key,
+                "num": dict[key]
+            }
+        )
+    return list_of_char
+
+def sort_on(dict):
+    return dict["num"]
 main()
