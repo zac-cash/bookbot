f4501b391b7cfc7263407bb1fc789df73dc195f2
zac-cash <Personal@zac.cash>
2024-09-02 14:20:41 -0500

    refactored word count into a function.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 path_to_file = './books/frankenstein.txt'
 
+def Word_Count (string):
+    return len(string.split())
+
 def main():
     with open(path_to_file) as f:
         filecontents = f.read()
-    print(filecontents)
+    w_count = Word_Count(filecontents)
+    print(f"there are {w_count} words in frankenstein.txt")
 
 main()
