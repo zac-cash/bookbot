edcc5c35e8d65d73aa2ed5abc7f48b2c8597e2ef
zac-cash <Personal@zac.cash>
2024-09-02 15:22:50 -0500

    JK on step 10. meant step 12. oh, btw step 12 is the end of project. small refactoring on this commit to end project.

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ path_to_file = './books/frankenstein.txt'
 
 def main():
     bookcontents = Get_Text(path_to_file)
-    char_list = Create_Char_List(Char_Count(bookcontents))
-    char_list.sort(reverse=True,key=sort_on)
+    char_list_of_dict = Create_Char_List(Char_Count(bookcontents))
 
     print(f"Sup. Welcome to the book report. \nWe are working on the file: {path_to_file.split('/')[-1]}\n")
     print(f"There are {Word_Count(Get_Text(path_to_file))} words in the book.")
+    
     print("The following is a count of each letter in the book:")
-    for char_dict in char_list:
+    for char_dict in char_list_of_dict:
         if char_dict['char'].isalpha():
             print(f"The '{char_dict['char']}' was found {char_dict['num']} times.")
     print("--- End Book report ---")
 
 
 def Get_Text (filepath):
-        with open(path_to_file) as f:
-            filecontents = f.read()
-        return filecontents
+    with open(path_to_file) as f:
+        return f.read()
+
 
 def Word_Count (string):
     return len(string.split())
@@ -38,8 +38,8 @@ def Create_Char_List (dict):
             {
                 "char": key,
                 "num": dict[key]
-            }
-        )
+            })
+    list_of_char.sort(reverse=True,key=sort_on)
     return list_of_char
 
 def sort_on(dict):
